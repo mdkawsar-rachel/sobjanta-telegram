e4fb601b8642582a8f35b77f171f50babe9f59bf
mkasajim <63280801+mkasajim@users.noreply.github.com>
2022-08-22 20:56:18 +0600

    Update amadeus.py

--- amadeus.py
+++ amadeus.py
@@ -220,8 +220,8 @@ def ask(username, botname, question, chat_log=None):
     t = '[' + ampm + '] '
     prompt = f'{chat_log}{t}{username}: {question}\n{t}{botname}:'
     response = completion.create(
-        prompt=prompt, engine="text-davinci-002", temperature=0.9,
-        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=3,
+        prompt=prompt, engine="text-davinci-002", temperature=0.3,
+        top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0, best_of=1,
         max_tokens=250)
     answer = response.choices[0].text
     return answer
